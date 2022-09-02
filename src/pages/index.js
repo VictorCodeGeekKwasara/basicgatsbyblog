6706d94ce43e6fb1ae3bc38cf996960a631cd785
professor_cypher <professorcodegeek@DESKTOP-87DPNVK.localdomain>
2022-09-02 18:45:03 +0200

    Using gatsby plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import * as React from 'react'
-import {Link} from 'gatsby'
 import Layout from '../components/layout'
+import {StaticImage} from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>
-        I'm making this by following the Gatsby Tutorial.
-      </p>
-    </Layout>
-  )
+		<Layout pageTitle='Home Page'>
+			<p>I'm making this by following the Gatsby Tutorial.</p>
+			<StaticImage
+				alt='Clifford, a reddish-brown ptbull'
+				src='../images/images.jpg'
+			/>
+		</Layout>
+	);
 }
 
 // You'll learn about this in the next task, just copy it for now
