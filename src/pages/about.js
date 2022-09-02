0c7326ff20f9b5c4101db58772c89283e72fb6d8
root <root@DESKTOP-87DPNVK.localdomain>
2022-09-02 15:13:49 +0200

    Layout components and styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 export default () => {
-	return <h1>Hie I am victor , I like talking about myself.</h1>;
+	return <Layout pageTitle="About Me"> 
+          <h1>Hie I am victor , I like talking about myself.</h1>
+         </Layout> ;
 };
 export const Head = () => <title>About</title>;
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
 import * as React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>
-        Welcome to my Gatsby site!
-      </h1>
+    <Layout pageTitle="Home Page">
       <p>
         I'm making this by following the Gatsby Tutorial.
       </p>
-    </main>
+    </Layout>
   )
 }
 
