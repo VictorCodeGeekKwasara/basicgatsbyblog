73948c27a1a4eff5f43d4cfc99672515cbfbd8b4
root <root@DESKTOP-87DPNVK.localdomain>
2022-09-04 21:10:37 +0200

    using mdx tranformer plugin

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,25 +6,32 @@ import Seo from '../components/seo';
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map( node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+      
+        {data.allMdx.nodes.map( node => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))}
-      </ul>
+      
     </Layout>
   )
 }
 
-export const pagesQuery =graphql`
- {
-  allFile {
-    nodes {
-      name
-    }
-  }
-}
-`
+export const pagesQuery = graphql`
+	{
+		allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+			nodes {
+				frontmatter {
+					title
+					date(formatString: "MMMM D, YYYY")
+				}
+				id
+				excerpt(pruneLength: 20)
+			}
+		}
+	}
+`;
 export const Head = () => <Seo title="My Blog Posts"/>
 export default BlogPage
